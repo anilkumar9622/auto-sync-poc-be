66f0b2d645ab976d169a84329495ecb1b9f8f967
Anil Kumar <anilchauhan.src@gmail.com>
2024-01-31 12:15:01 +0530

    multiple data store at a time

--- index.js
+++ index.js
@@ -12,18 +12,32 @@ const allowedOrigins = [
 app.use(cors({
   origin: "*",
 }));
-  app.post("/create", async (req, res) => {
-      try{
-        const data = req.body
-        await User.add({data});
-        res.send({msg : "User added"})
-      }
-      catch(error){
-          console.log(error);
-       res.send("err")
-      }
+  // app.post("/create", async (req, res) => {
+  //     try{
+  //       const data = req.body
+  //       await User.add({data});
+  //       res.send({msg : "User added"})
+  //     }
+  //     catch(error){
+  //         console.log(error);
+  //      res.send("err")
+  //     }
   
-  })
+  // })
+
+  app.post("/create", async (req, res) => {
+    try {
+        const dataList = req.body; // Assuming req.body is an array of data objects
+        await Promise.all(dataList.map(async (data) => {
+            await User.add(data); // Assuming User.add is an asynchronous operation
+        }));
+        res.send({ msg: "Users added" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+});
+
 
   app.listen(4000, ()=>console.log("Server running on port 4000"))
 
